3d36d5e487f3a836331d2ebd7c988f769e6a4db0
nabeer93 <nabeel.anees@sols-pro.com>
2024-02-01 20:58:56 +0500

    upload code

---
+++ src/data/site.ts
@@ -0,0 +1,16 @@
+export default {
+  // website description
+  description: `The ultimate Encrypter from the Past.`,
+  // contact email
+  email: `faixan1987@gmail.com`,
+  // site url
+  url: ``,
+  // author url
+  authorURL: `https://github.com/faixysaati`,
+  // source code url
+  sourcecodeURL: `https://github.com/faixysaati`,
+  // github url
+  githubURL: `https://github.com/faixysaati`,
+  // discord server url
+  discordURL: ``,
+};
